b22647c5f93abfb6883ff1cbdb81cc02690c8b01
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-01-17 09:35:43 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,9 +3,9 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		int i = 2;
-  	while(isPrime(Double.parseDouble(e.substring(i,i+10)))==false){
-  		i++;
+		private int i = 2;
+  		while(isPrime(Double.parseDouble(e.substring(i,i+10)))==false){
+  			i++;
  		}
 		System.out.println(e.substring(i,i+10));
 	}
@@ -17,7 +17,7 @@ public class GoogleBillboard {
 			return false;
 		for(int i = 2; i<=Math.sqrt(dNum); i++){
  			if(dNum%i==0)
-    		return false;
+    				return false;
 		}
 		return true; 
 	}
